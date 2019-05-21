cdb8474bbf96be8364b272d59eee9c84da1a7d08
arbisoft <awais.qureshi@arbisoft.com>
2019-05-21 15:18:00 +0500

    Making package.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import find_packages, setup
+
+
+setup(
+    name='resume_parser',
+    version='0.0.1',
+    description='A utility to make parse data from resumes.',
+    packages=['resume'],
+    install_requires=[
+        'gensim==3.7.1',
+        'pandas==0.24.2',
+        'pdfminer.six==20181108',
+        'spacy==2.1.3',
+        'PyYAML==5.1'
+    ],
+)
